47995f6ec35ff63c8571ad7c6f69fcb0fd3283a1
Salomon Muriel <salomon.muriel@gmail.com>
2020-06-02 00:32:17 -0500

    Banner image centering

--- src/components/ui/Banner/styles.ts
+++ src/components/ui/Banner/styles.ts
@@ -10,9 +10,9 @@ export const Content = styled.p`
 `;
 
 export const Image = styled.figure`
-  ${tw`-my-12 sm:w-0 md:w-1/3`};
+  ${tw`sm:w-0 md:w-4/12 my-auto mx-auto`};
 `;
 
 export const LeftContent = styled.p`
-  ${tw`flex flex-col sm:w-full md:w-2/3`};
+  ${tw`flex flex-col sm:w-full md:w-7/12`};
 `;
